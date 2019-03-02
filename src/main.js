73582b9952d51668bb8d49901aa9db870a97e413
Miles Exner <dizolivemint@gmail.com>
2019-03-02 13:57:40 -0800

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import VCalendar from 'v-calendar'
 import 'v-calendar/lib/v-calendar.min.css'
 import 'babel-polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 

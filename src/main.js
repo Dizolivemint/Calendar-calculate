3868b7c343df5d7f3136c0ab794ef8b75f2e570b
Miles Exner <dizolivemint@gmail.com>
2019-03-02 13:43:41 -0800

    Add Vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import VCalendar from 'v-calendar'
+import 'v-calendar/lib/v-calendar.min.css'
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
+
+// Use v-calendar, v-date-picker & v-popover components
+Vue.use(VCalendar, {
+  firstDayOfWeek: 2  // Monday
+});
 
 new Vue({
   router,
